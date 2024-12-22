fde3b20457b22170bf1cad7b6b02c2182179470f
Ankush <rehalankush192@gmail.com>
2024-12-22 21:16:09 +0530

    feat:added the navbar

--- Frontend/Heart_predection/src/components/Navbar.jsx
+++ Frontend/Heart_predection/src/components/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
+  const homeList = ['Home', 'Feature', 'Resource',];
+
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className="flex justify-between p-4 px-10">
+        <div className='w-[50%] text-lg font-semibold' >
+          HealthAI
+        </div>
+        <div className='w-[50%]'>
+          <ul className='flex justify-around'>
+            {homeList.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+            <button className='bg-pink-500'>SignIn</button>
+          </ul>
+        </div>
+      </div>
+      <hr></hr>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
